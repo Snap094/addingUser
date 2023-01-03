b498973e08af4a7e1584ea455b9f16f0b7ac519e
nikonchuk.pp <peiko.nikonchuk.p@gmail.com>
2023-01-03 16:43:26 +0200

    add mobile main image

--- src/components/FirstSection.vue
+++ src/components/FirstSection.vue
@@ -39,6 +39,11 @@
             overflow-y: auto;
             padding-top: 10px;
             padding-bottom: 10px;
+
+            @include media-sm {
+                background: linear-gradient(0deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('@/assets/images/baza-mob.png') center / cover no-repeat;
+
+            }
         }
 
         &__content {
